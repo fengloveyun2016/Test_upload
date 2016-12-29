f173fdc02e5cb68e51184b5857aceae39b1b32c9
fengloveyun2016 <82213492@qq.com>
2016-12-29 15:49:58 +0800

    修改
    修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-	'DB_TYPE'               =>  'mysql',     // 数据库类型
-	'DB_HOST'               =>  'thinkphp.52tty.com', // 服务器地址
-	'DB_NAME'               =>  'thinkphp',          // 数据库名
-	'DB_USER'               =>  'thinkphp',      // 用户名
-	'DB_PWD'                =>  'admin888',          // 密码
+	'DB_TYPE'               =>  '',     // 数据库类型
+	'DB_HOST'               =>  '', // 服务器地址
+	'DB_NAME'               =>  '',          // 数据库名
+	'DB_USER'               =>  '',      // 用户名
+	'DB_PWD'                =>  '',          // 密码
 	'DB_PORT'               =>  '',        // 端口
 	'DB_PREFIX'             =>  '',    // 数据库表前缀
 	'DB_PARAMS'          	=>  array(), // 数据库连接参数
